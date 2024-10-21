ad4aa496496ce0993f40e57bdd780c2138b623a7
Sandrew17 <sanchezandrew1734@gmail.com>
2024-10-21 18:29:26 -0400

    base game working

--- Ascend and annihilate/LOneWorld.java
+++ Ascend and annihilate/LOneWorld.java
@@ -14,6 +14,24 @@ public class LOneWorld extends World
      */
     public LOneWorld()
     {
-        super(600, 400, 1);
+        super(830, 800, 1);
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Player player =  new Player();
+        addObject(player, 226, 207);
+        Pistol pistol =  new Pistol();
+        addObject(pistol, 263, 203);
+        pistol.setLocation(245, 204);
+        Pistol pistol2 =  new Pistol();
+        addObject(pistol2, 115, 357);
+        Pistol pistol3 =  new Pistol();
+        addObject(pistol3, 460, 550);
     }
 }
--- Ascend and annihilate/Player.java
+++ Ascend and annihilate/Player.java
@@ -10,9 +10,73 @@ public class Player extends Actor
 {
 
     /**
-     * Act - do whatever the Player wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     * 
      */
     public void act()
     {
+        look();
+        move();
+        swing();
+    }
+
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void look()
+    {
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if (mouse != null) {
+            turnTowards(mouse.getX(), mouse.getY());
+        }
+    }
+
+    /**
+     * 
+     */
+    public void move()
+    {
+        if (Greenfoot.isKeyDown("w")) {
+            setLocation(getX(), getY() - 2);
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            setLocation(getX() - 2, getY());
+        }
+        if (Greenfoot.isKeyDown("s")) {
+            setLocation(getX(), getY() + 2);
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            setLocation(getX() + 2, getY());
+        }
+        if (Greenfoot.isKeyDown("mouse1")) {
+            setLocation(getX() + 2, getY());
+        }
+    }
+
+    /**
+     * 
+     */
+    public void swing()
+    {
+        if (Greenfoot.mouseClicked(null)) {
+            swingBat();
+        }
+    }
+
+    /**
+     * 
+     */
+    private void swingBat()
+    {
+        int direction = getRotation();
+        List<Actor> actorsInRange = getObjectsInRange(60, Actor.class);
+        for (final Actor actor : actorsInRange) {
+            if (actor != this) {
+                int angleToActor = (int)Math.toDegrees(Math.atan2(actor.getY() - getY(), actor.getX() - getX()));
+                int angleDifference = Math.abs((angleToActor - direction + 360) % 360);
+                if ((angleDifference <= 40 || angleDifference >= (360 - 20))) {
+                    getWorld().removeObject(actor);
+                }
+            }
+        }
     }
 }
