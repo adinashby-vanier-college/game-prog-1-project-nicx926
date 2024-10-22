bbc72e12038a9e2a8d0fe814e8d4688e0ab9a5e1
nicx926 <nicholashu2007@gmail.com>
2024-10-22 09:51:00 -0400

    added more strides

--- Ascend and annihilate/LOneWorld.java
+++ Ascend and annihilate/LOneWorld.java
@@ -24,14 +24,43 @@ public class LOneWorld extends World
      */
     private void prepare()
     {
-        Player player =  new Player();
+        Player player =  new  Player();
         addObject(player, 226, 207);
-        Pistol pistol =  new Pistol();
-        addObject(pistol, 263, 203);
-        pistol.setLocation(245, 204);
-        Pistol pistol2 =  new Pistol();
-        addObject(pistol2, 115, 357);
-        Pistol pistol3 =  new Pistol();
-        addObject(pistol3, 460, 550);
+        Enemy enemy =  new  Enemy();
+        addObject(enemy, 296, 299);
+        enemy.setLocation(302, 430);
+        enemy.setLocation(208, 384);
+        enemy.setLocation(120, 547);
+        enemy.setLocation(157, 536);
+        enemy.setLocation(131, 689);
+        enemy.setLocation(146, 371);
+        enemy.setLocation(149, 402);
+        deadSecurity deadSecurity =  new  deadSecurity();
+        addObject(deadSecurity, 529, 556);
+        deadSecurity.setLocation(539, 542);
+        deadSecurity.setLocation(159, 426);
+        deadSecurity.setLocation(160, 432);
+        deadSecurity.setLocation(176, 437);
+        removeObject(deadSecurity);
+        addObject(deadSecurity, 190, 433);
+        deadSecurity.setLocation(139, 375);
+        deadSecurity.setLocation(149, 365);
+        deadSecurity.setLocation(154, 386);
+        deadSecurity.setLocation(189, 383);
+        removeObject(deadSecurity);
+        enemy.setLocation(165, 459);
+        worker worker =  new  worker();
+        addObject(worker, 440, 340);
+        worker worker2 =  new  worker();
+        addObject(worker2, 655, 587);
+        worker worker3 =  new  worker();
+        addObject(worker3, 267, 644);
+        Enemy enemy2 =  new  Enemy();
+        addObject(enemy2, 349, 390);
+        enemy.setLocation(152, 385);
+        enemy2.setLocation(353, 377);
+        worker.setLocation(418, 291);
+        worker2.setLocation(618, 564);
+        worker3.setLocation(376, 603);
     }
 }
